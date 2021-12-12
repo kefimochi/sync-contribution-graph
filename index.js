1c5acca61746d2d71c2754a99646f64f662da135
Kate Efimova <kate.efimova0@gmail.com>
2021-12-12 13:19:25 -0800

    Added an interactive user interface

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 import { parse } from "node-html-parser";
-import dotenv from "dotenv";
 import axios from "axios";
 
-// We need this since the username against which a sync will be
-// performed is provided an .env input.
-dotenv.config();
+export default async (username, year) => {
+  const res = await axios.get(
+    `https://github.com/users/${username}/contributions?tab=overview&from=${year}-12-01&to=${year}-12-31`
+  );
 
-const res = await axios.get(
-  `https://github.com/users/${process.env.GITHUB_USERNAME}/contributions`
-);
+  // Gathers all the squares from GitHub contribution graph.
+  const allSquares = parse(res.data).querySelectorAll("[data-count]");
 
-// Gathers all the squares from GitHub contribution graph.
-const allSquares = parse(res.data).querySelectorAll("[data-count]");
+  // We only want html elements of squares with 1+ commits/issues for that day.
+  const relevantContributions = allSquares
+    .filter((value) => value._rawAttrs["data-count"] > 0)
+    .map((filteredSquare) => {
+      return {
+        date: filteredSquare._rawAttrs["data-date"],
+        count: filteredSquare._rawAttrs["data-count"],
+      };
+    });
 
-// We only want html elements of squares with 1+ commits/issues for that day.
-const relevantContributions = allSquares
-  .filter((value) => value._rawAttrs["data-count"] > 0)
-  .map((filteredSquare) => {
-    return {
-      date: filteredSquare._rawAttrs["data-date"],
-      count: filteredSquare._rawAttrs["data-count"],
-    };
+  let fullCommand = "";
+
+  relevantContributions.forEach((contribution) => {
+    for (let i = 0; i < contribution.count; i++) {
+      fullCommand += `GIT_AUTHOR_DATE=${contribution.date}T12:00:00 GIT_COMMITER_DATE=${contribution.date}T12:00:00 git commit --allow-empty -m "Rewriting History!" > /dev/null;`;
+    }
   });
 
-let fullCommand = "";
-
-relevantContributions.forEach((contribution) => {
-  for (let i = 0; i < contribution.count; i++) {
-    fullCommand += `GIT_AUTHOR_DATE=${contribution.date}T12:00:00 GIT_COMMITER_DATE=${contribution.date}T12:00:00 git commit --allow-empty -m "Rewriting History!" > /dev/null;`;
-  }
-});
-
-console.log(fullCommand);
+  console.log(fullCommand);
+};
---
+++ interface.js
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+import script from "./index.js";
+import axios from "axios";
+
+console.log("\nHello there!\n");
+
+const questions = [
+  {
+    type: "input",
+    name: "nickname",
+    message:
+      "Please enter GitHub nickname with which you'd like to sync contributions:",
+    validate: (value) =>
+      axios
+        .get(`https://api.github.com/users/${value}`)
+        .then(() => true)
+        .catch(() => "Please enter an existing GitHub username."),
+  },
+  {
+    type: "input",
+    name: "year",
+    message: "What year would you like to sync?",
+    default() {
+      return new Date().getFullYear();
+    },
+  },
+  {
+    type: "confirm",
+    name: "confirm",
+    message: "Ready to push generated commits to remote origin?",
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  console.log(JSON.stringify(answers, null, "  "));
+  if (answers.confirm) {
+    script(answers.nickname, answers.year);
+  }
+  console.log("\nExiting...");
+});
