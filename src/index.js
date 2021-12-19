9daccc2f4df571b6eb11f77e026ca0fffc167d65
Kate Efimova <kate.efimova0@gmail.com>
2021-12-19 15:04:19 -0800

    Modify minor details plus add extra comments

--- src/index.js
+++ src/index.js
@@ -3,20 +3,23 @@ import axios from "axios";
 import fs from "fs";
 import shell from "shelljs";
 
-function commits_for(contribution) {
+// Gathers needed git commands for bash to execute per provided contribution data.
+const getCommand = (contribution) => {
   return `GIT_AUTHOR_DATE=${contribution.date}T12:00:00 GIT_COMMITER_DATE=${contribution.date}T12:00:00 git commit --allow-empty -m "Rewriting History!" > /dev/null\n`.repeat(contribution.count)
 }
 
 export default async (input) => {
+  // Returns contribution graph html for a full selected year.
   const res = await axios.get(
     `https://github.com/users/${input.username}/contributions?tab=overview&from=${input.year}-12-01&to=${input.year}-12-31`
   );
 
-  // Gathers all the squares from GitHub contribution graph.
+  // Retrives needed data from the html, loops over green squares with 1+ contributions,
+  // and produces a multi-line string that can be run as a bash command.
   const script = parse(res.data).querySelectorAll("[data-count]")
     .map(el => { return {date: el.attributes["data-date"], count: parseInt(el.attributes["data-count"])}} )
     .filter(contribution => contribution.count > 0)
-    .map(contribution => commits_for(contribution))
+    .map(contribution => getCommand(contribution))
     .join("")
     .concat(
       "git pull origin main\n",
